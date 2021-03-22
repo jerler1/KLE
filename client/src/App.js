28dfff373062f045c17c98f4c92ce62bcb6e0054
Bunks <busk_1@yahoo.com>
2021-03-22 15:58:26 -0400

    initial files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Is this skia?
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <main className="App">
+        <Switch>
+          
+        </Switch>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return <div>
+      This is home.
+  </div>;
+};
+
+export default Home;
