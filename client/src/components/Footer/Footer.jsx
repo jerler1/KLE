d1118eeff22546047940c311acd96c5a170bcaf6
Bunks <busk_1@yahoo.com>
2021-03-22 21:22:31 -0400

    added business info to the footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer>
+      <section className="footerContainer">
+        <article className="businessInformation">
+          <p>Kinetic Lighting and Electric CA C10 License #838279</p>
+          <p>Based in Mill Valley, CA Serving Marin, SF and the East Bay</p>
+          <p>415.569.7167</p>
+          <p>hello@kineticelectricsf.com</p>
+        </article>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
