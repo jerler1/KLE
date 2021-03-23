e0889eb2d1003e23f77c174ff8b1d3550b2cd608
Bunks <busk_1@yahoo.com>
2021-03-22 23:16:47 -0400

    added white phone and email

---
+++ client/src/assets/index.js
@@ -0,0 +1,12 @@
+// Icons
+export const email = require("./icons/email.png").default;
+export const instagram = require("./icons/instagram.png").default;
+export const phone = require("./icons/phone.png").default;
+export const podcast = require("./icons/podcast.png").default;
+export const yelp = require("./icons/yelp.png").default;
+
+// Images
+export const background = require("./images/Background.png").default;
+export const pic1 = require("./images/pic1-SanAnselmo.png").default;
+export const pic2 = require("./images/pic2-Ellsworth.jpg").default;
+export const pic3 = require("./images/pic3-SanAnselmo.png").default;
