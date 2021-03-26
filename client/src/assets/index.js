a084da28308b51a4c1b797958463bce7c70dc847
Bunks <busk_1@yahoo.com>
2021-03-26 18:22:29 -0400

    added arrow down

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,6 +4,7 @@ export const instagram = require("./icons/instagram.png").default;
 export const phone = require("./icons/phone.png").default;
 export const podcast = require("./icons/podcast.png").default;
 export const yelp = require("./icons/yelp.png").default;
+export const arrowDown = require("./icons/arrow-down.png").default;
 
 // Images
 export const background = require("./images/Background.png").default;
