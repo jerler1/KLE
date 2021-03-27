c9b2413c802970e085d8acf2cdfc5a9904cf9ab1
Bunks <busk_1@yahoo.com>
2021-03-27 17:57:17 -0400

    added toggles for the drop down menu

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import { arrowDown } from "../../assets/index";
 
 const Navbar = () => {
+  const [toggleMenuOne, setToggleMenuOne] = useState(false);
+  const [toggleMenuTwo, setToggleMenuTwo] = useState(false);
+  const [toggleMenuThree, setToggleMenuThree] = useState(false);
+
+  const toggleMenu = (event) => {
+    console.log(event.target);
+  }
+
   return (
     <nav className="navbarContainer">
       <div className="brand-logo"></div>
@@ -10,40 +19,40 @@ const Navbar = () => {
         <div className="navWrapper">
           <ul className="navDropdown">
             <li>
-              <button href="#">
+              <button onClick={toggleMenu} name="menuOne">
                 Our Work
                 <span>
                   <img src={arrowDown} alt="arrow down" />
                 </span>
               </button>
-              <div className="menu menuOne">
-                <a href="#">Why Us</a>
-                <a href="#">Portfolio</a>
+              <div className={toggleMenuOne ? "menu menuOne active" : "menu menuOne"}>
+                <Link to="/">Why Us</Link>
+                <Link to="/">Portfolio</Link>
               </div>
             </li>
             <li>
-              <button href="#">
+              <button onClick={toggleMenu} name="menuTwo">
                 About Us
                 <span>
                   <img src={arrowDown} alt="arrow down" />
                 </span>
               </button>
-              <div className="menu menuTwo">
-                <a href="#">Who We Are</a>
-                <a href="#">Our Services</a>
-                <a href="#">Join Our Team</a>
+              <div className={toggleMenuTwo ? "menu menuTwo active" : "menu menuTwo"}>
+                <Link to="/">Who We Are</Link>
+                <Link to="/">Our Services</Link>
+                <Link to="/">Join Our Team</Link>
               </div>
             </li>
             <li>
-              <button href="#">
+              <button onClick={toggleMenu} name="menuThree">
                 Resources
                 <span>
                   <img src={arrowDown} alt="arrow down" />
                 </span>
               </button>
-              <div className="menu menuThree">
-                <a href="#">FAQs</a>
-                <a href="#">Our Partners</a>
+              <div className={toggleMenuThree ? "menu menuThree active" : "menu menuThree"}>
+                <Link to="/">FAQs</Link>
+                <Link to="/">Our Partners</Link>
               </div>
             </li>
           </ul>
