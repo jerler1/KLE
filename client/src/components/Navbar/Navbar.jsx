3a68a3b189c2168862f7b627d46e9cc93d6a7a0d
Bunks <busk_1@yahoo.com>
2021-03-28 16:04:05 -0400

    making mobile navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import "./Navbar.scss";
 import { arrowDown } from "../../assets/index";
 
 const Navbar = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1023px)" });
+
   const [toggleMenuState, setToggleMenuState] = useState({
     menuOne: false,
     menuTwo: false,
@@ -25,96 +28,106 @@ const Navbar = () => {
     value = JSON.parse(value);
     setToggleMenuState({ ...toggleMenuState, [name]: !value });
   };
-  const closeMenu = useCallback((event) => {
-    if (menu.current && !menu.current.contains(event.target)) {
-      setToggleMenuState({ menuOne: false, menuTwo: false, menuThree: false });
-      document.removeEventListener("click", closeMenu);
-    }
-  }, [menu]);
+  const closeMenu = useCallback(
+    (event) => {
+      if (menu.current && !menu.current.contains(event.target)) {
+        setToggleMenuState({
+          menuOne: false,
+          menuTwo: false,
+          menuThree: false,
+        });
+        document.removeEventListener("click", closeMenu);
+      }
+    },
+    [menu]
+  );
 
   return (
-    <nav className="navbarContainer">
-      <div className="brand-logo"></div>
-      <div className="nav-links">
-        <div className="navWrapper">
-          <ul className="navDropdown">
-            <li>
-              <button
-                onClick={toggleMenu}
-                name="menuOne"
-                value={toggleMenuState.menuOne}
-              >
-                Our Work
-                <span>
-                  <img src={arrowDown} alt="arrow down" />
-                </span>
-              </button>
-              <div
-                ref={menu}
-                className={
-                  toggleMenuState.menuOne
-                    ? "menu menuOne active"
-                    : "menu menuOne"
-                }
-              >
-                <Link to="/">Why Us</Link>
-                <Link to="/">Portfolio</Link>
-              </div>
-            </li>
-            <li>
-              <button
-                onClick={toggleMenu}
-                name="menuTwo"
-                value={toggleMenuState.menuTwo}
-              >
-                About Us
-                <span>
-                  <img src={arrowDown} alt="arrow down" />
-                </span>
-              </button>
-              <div
-                ref={menu}
-                className={
-                  toggleMenuState.menuTwo
-                    ? "menu menuTwo active"
-                    : "menu menuTwo"
-                }
-              >
-                <Link to="/">Who We Are</Link>
-                <Link to="/">Our Services</Link>
-                <Link to="/">Join Our Team</Link>
-              </div>
-            </li>
-            <li>
-              <button
-                onClick={toggleMenu}
-                name="menuThree"
-                value={toggleMenuState.menuThree}
-              >
-                Resources
-                <span>
-                  <img src={arrowDown} alt="arrow down" />
-                </span>
-              </button>
-              <div
-                ref={menu}
-                className={
-                  toggleMenuState.menuThree
-                    ? "menu menuThree active"
-                    : "menu menuThree"
-                }
-              >
-                <Link to="/">FAQs</Link>
-                <Link to="/">Our Partners</Link>
-              </div>
-            </li>
-          </ul>
-          <div className="contactUsButton">
-            <button>Contact Us</button>
-          </div>
-        </div>
-      </div>
-    </nav>
+      <nav className="mobileNavbarContainer">
+
+      </nav>
+    // <nav className="navbarContainer">
+    //   <div className="brand-logo"></div>
+    //   <div className="nav-links">
+    //     <div className="navWrapper">
+    //       <ul className="navDropdown">
+    //         <li>
+    //           <button
+    //             onClick={toggleMenu}
+    //             name="menuOne"
+    //             value={toggleMenuState.menuOne}
+    //           >
+    //             Our Work
+    //             <span>
+    //               <img src={arrowDown} alt="arrow down" />
+    //             </span>
+    //           </button>
+    //           <div
+    //             ref={menu}
+    //             className={
+    //               toggleMenuState.menuOne
+    //                 ? "menu menuOne active"
+    //                 : "menu menuOne"
+    //             }
+    //           >
+    //             <Link to="/">Why Us</Link>
+    //             <Link to="/">Portfolio</Link>
+    //           </div>
+    //         </li>
+    //         <li>
+    //           <button
+    //             onClick={toggleMenu}
+    //             name="menuTwo"
+    //             value={toggleMenuState.menuTwo}
+    //           >
+    //             About Us
+    //             <span>
+    //               <img src={arrowDown} alt="arrow down" />
+    //             </span>
+    //           </button>
+    //           <div
+    //             ref={menu}
+    //             className={
+    //               toggleMenuState.menuTwo
+    //                 ? "menu menuTwo active"
+    //                 : "menu menuTwo"
+    //             }
+    //           >
+    //             <Link to="/">Who We Are</Link>
+    //             <Link to="/">Our Services</Link>
+    //             <Link to="/">Join Our Team</Link>
+    //           </div>
+    //         </li>
+    //         <li>
+    //           <button
+    //             onClick={toggleMenu}
+    //             name="menuThree"
+    //             value={toggleMenuState.menuThree}
+    //           >
+    //             Resources
+    //             <span>
+    //               <img src={arrowDown} alt="arrow down" />
+    //             </span>
+    //           </button>
+    //           <div
+    //             ref={menu}
+    //             className={
+    //               toggleMenuState.menuThree
+    //                 ? "menu menuThree active"
+    //                 : "menu menuThree"
+    //             }
+    //           >
+    //             <Link to="/">FAQs</Link>
+    //             <Link to="/">Our Partners</Link>
+    //           </div>
+    //         </li>
+    //       </ul>
+    //       <div className="contactUsButton">
+    //         <button>Contact Us</button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 
