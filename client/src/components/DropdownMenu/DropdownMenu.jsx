8c4be53f9b1f4982273296810be37d84974abdd1
Bunks <busk_1@yahoo.com>
2021-04-05 12:55:58 -0400

    drop down menu component tentatively done

--- client/src/components/DropdownMenu/DropdownMenu.jsx
+++ client/src/components/DropdownMenu/DropdownMenu.jsx
@@ -1,50 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
+import { useDetectOutsideClick } from "../../hooks/useDetectOutsideClick";
 import { arrowDown } from "../../assets/index";
 import "./DropdownMenu.scss";
 
-const DropdownMenu = ({ children }) => {
+const DropdownMenu = ({ children, message }) => {
   const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
-  const onClick = () => setIsActive(!isActive);
-
-  useEffect(() => {
-    const pageClickEvent = (event) => {
-      if (
-        dropdownRef.current !== null &&
-        !dropdownRef.current.contains(event.target)
-      ) {
-        setIsActive(!isActive);
-      }
-    };
-
-    if (isActive) {
-      window.addEventListener("click", pageClickEvent, true);
-    }
-
-    return () => {
-      window.removeEventListener("click", pageClickEvent, true);
-    };
-  }, [isActive]);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => {
+    setIsActive(!isActive);
+  };
 
   return (
     <li className="menu-container">
-      <button
-        onClick={onClick}
-        className="menu-trigger"
-        name="menuTwo"
-        // value={isMenuTwoActive}
-      >
-        About Us
+      <button onClick={onClick} name="menuOne" value={isActive}>
+        {message}
         <span>
           <img src={arrowDown} alt="arrow down" />
         </span>
       </button>
-      <nav
+      <div
         ref={dropdownRef}
         className={`menu ${isActive ? "active" : "inactive"}`}
       >
         {children}
-      </nav>
+      </div>
     </li>
   );
 };
