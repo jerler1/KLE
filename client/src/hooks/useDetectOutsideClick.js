691ffa7dd3b6f91185181d4171487886c559d07d
Bunks <busk_1@yahoo.com>
2021-04-05 12:42:35 -0400

    added outside click hook

---
+++ client/src/hooks/useDetectOutsideClick.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useDetectOutsideClick = (element, intialState) => {
+  const [isActive, setIsActive] = useState(initialState);
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      // If the active element exists and is clicked outside of
+      if (el.current !== null && !el.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    };
+
+    // If the item is active (ie open) then listen for clicks
+    if (isActive) {
+      window.addEventListener("click", pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [isActive, element]);
+
+  return [isActive, setIsActive];
+};
