bc18bbbb4aa56c8974d4f870c9cc9ffe3853a22b
Bunks <busk_1@yahoo.com>
2021-04-06 18:49:46 -0400

    finished icon component to import icons dynamically

---
+++ client/src/components/Icons/Icons.jsx
@@ -0,0 +1,27 @@
+// https://dev.to/barrbozzo/the-right-way-to-use-svg-icons-with-react-30o3
+import React, { useEffect, useState } from "react";
+
+const Icons = ({ name, ...otherProps }) => {
+  const [iconModule, setIconModule] = iseState(null);
+
+  useEffect(() => {
+    import(`../../assets/icons/${name}.svg`)
+      .then((module) => {
+        setIconModule(module);
+      })
+      .catch((error) => {
+        console.error(`Icon with name: ${name} not found!`);
+      });
+  }, [name]);
+
+  const renderIcon = () => {
+    if (!iconModule) return null;
+
+    const Component = iconModule.ReactComponent;
+
+    return <Component {...otherProps} />;
+  };
+  return <>{renderIcon()}</>;
+};
+
+export default Icons;
