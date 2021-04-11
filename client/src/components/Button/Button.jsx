a84cc33a3b3e8951e72dd2f4f59cb9873afe0ab8
Bunks <busk_1@yahoo.com>
2021-04-11 00:28:13 -0400

    added on click events to the button component

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Button.scss";
 
-const Button = ({ className, children }) => {
-  return <button className={className}>{children}</button>;
+const Button = ({ className, children, onClick }) => {
+  return <button className={className} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
