cfea898160944ed80ea72effcd70a49535a990ef
Bunks <busk_1@yahoo.com>
2021-04-13 22:47:25 -0400

    not working due to dynamic import

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./PageHeader.scss";
+import { pic3 } from "../../assets/index";
 
 const PageHeader = ({ message, url }) => {
+    const image = require(`../../assets/images/pic3-SanAnselmo.png`);
   const headerImageStyles = {
-    backgroundImage: `linear-gradient(to bottom right, transparent 50%, rgba(var(--mainForegroundColor), 0.8) 50%), url(${url});`,
+    backgroundImage: `linear-gradient(to bottom right, transparent 50%, rgba(var(--mainForegroundColor), 0.8) 50%), url(${image});`
   };
 
   return (
