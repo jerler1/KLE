7f9acf5800e01b8c9faf77ea02fbd6d8c584ac99
Bunks <busk_1@yahoo.com>
2021-04-14 01:26:46 -0400

    added projects to the portfolio

--- client/src/views/Portfolio/Portfolio.jsx
+++ client/src/views/Portfolio/Portfolio.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Portfolio.scss";
-import PageHeader from "../../components/PageHeader/PageHeader";
-// import { pic3 } from "../../assets/index";
+// import PageHeader from "../../components/PageHeader/PageHeader";
+import { pic3 } from "../../assets/index";
 
 const Portfolio = () => {
   return (
     <div className="portfolioContainer">
       <section className="headerBackgroundImage">
+        {/* <img src={pic3} alt="kitchen well lighted"/> */}
         <header className="headerTitle">
           <h1>Portfolio</h1>
         </header>
       </section>
       {/* <PageHeader message="Portfolio" url={"pic3"} /> */}
-      <section></section>
+      <section className="projectGrid">
+        <div className="project">
+          <caption>
+            <h4>Elizabeth Street</h4>
+            <p>Kitchen Remodel</p>
+          </caption>
+        </div>
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+      </section>
       <section></section>
     </div>
   );
