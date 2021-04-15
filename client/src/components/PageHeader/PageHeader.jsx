ddd98c3b926111dfaea568d1697df85c06ee7abb
Bunks <busk_1@yahoo.com>
2021-04-14 20:59:32 -0400

    refactored out unused variables

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./PageHeader.scss";
-import { pic3 } from "../../assets/index";
 
-const PageHeader = ({ message, url, className }) => {
-
+const PageHeader = ({ message, className }) => {
   // const headerImageStyles = {
   //   backgroundImage: `linear-gradient(to bottom right, transparent 50%, rgba(var(--mainForegroundColor), 0.8) 50%), url(${url});`
   // };
 
   return (
-    <section style={headerImageStyles} className={`headerBackgroundImage ${className}`}>
+    <section className={`headerBackgroundImage ${className}`}>
       <header className="headerTitle">
         <h1>{message}</h1>
       </header>
