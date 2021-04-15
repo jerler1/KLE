260970876576b22feb049909a88c04c671d94f35
Bunks <busk_1@yahoo.com>
2021-04-14 21:00:45 -0400

    removed the image header code from the portfolio and moved it to component PageHeader

--- client/src/views/Portfolio/Portfolio.jsx
+++ client/src/views/Portfolio/Portfolio.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import "./Portfolio.scss";
-// import PageHeader from "../../components/PageHeader/PageHeader";
+import PageHeader from "../../components/PageHeader/PageHeader";
 import { pic3 } from "../../assets/index";
 
 const Portfolio = () => {
   return (
     <div className="portfolioContainer">
-      <section className="headerBackgroundImage">
-        {/* <img className="headerBackgroundImage" src={croppedHeader} alt="kitchen well lighted"/> */}
-        <header className="headerTitle">
-          <h1>Portfolio</h1>
-        </header>
-      </section>
-      {/* <PageHeader message="Portfolio" url={"pic3"} /> */}
+      <PageHeader message="Portfolio" className="portfolio" />
       <section className="projectGrid">
         <div className="project">
           <caption>
