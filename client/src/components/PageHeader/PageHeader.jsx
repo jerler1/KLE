098d74b8f1197a7ad7a13111bd25a72fba1d2e04
Bunks <busk_1@yahoo.com>
2021-04-14 20:56:41 -0400

    adding className based background as a work around

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./PageHeader.scss";
 import { pic3 } from "../../assets/index";
 
-const PageHeader = ({ message, url }) => {
-    const image = require(`../../assets/images/pic3-SanAnselmo.png`);
-  const headerImageStyles = {
-    backgroundImage: `linear-gradient(to bottom right, transparent 50%, rgba(var(--mainForegroundColor), 0.8) 50%), url(${image});`
-  };
+const PageHeader = ({ message, url, className }) => {
+
+  // const headerImageStyles = {
+  //   backgroundImage: `linear-gradient(to bottom right, transparent 50%, rgba(var(--mainForegroundColor), 0.8) 50%), url(${url});`
+  // };
 
   return (
-    <section style={headerImageStyles} className="headerBackgroundImage">
+    <section style={headerImageStyles} className={`headerBackgroundImage ${className}`}>
       <header className="headerTitle">
         <h1>{message}</h1>
       </header>
