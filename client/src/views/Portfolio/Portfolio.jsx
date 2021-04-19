b743922808009272bea8bdc2ff3e8a17fe18d4e7
Bunks <busk_1@yahoo.com>
2021-04-19 19:31:47 -0400

    added CFA pictures

--- client/src/views/Portfolio/Portfolio.jsx
+++ client/src/views/Portfolio/Portfolio.jsx
@@ -9,9 +9,42 @@ const Portfolio = () => {
     <div className="portfolioContainer">
       <PageHeader message="Portfolio" className="portfolio" />
       <section className="projectGrid">
-        {portfolioProjects.map((project) => {
-          <Project {...project} />;
-        })}
+        <div className="projectContainer">
+          <div className="project">
+            <img src={pic3} alt="lovely kitchen" />
+            <caption>
+              <h4>Elizabeth Street</h4>
+              <p>Kitchen Remodel</p>
+            </caption>
+          </div>
+        </div>
+        <div className="projectContainer">
+          <div className="project">
+            <img src={pic3} alt="lovely kitchen" />
+            <caption>
+              <h4>Elizabeth Street</h4>
+              <p>Kitchen Remodel</p>
+            </caption>
+          </div>
+        </div>
+        <div className="projectContainer">
+          <div className="project">
+            <img src={pic3} alt="lovely kitchen" />
+            <caption>
+              <h4>Elizabeth Street</h4>
+              <p>Kitchen Remodel</p>
+            </caption>
+          </div>
+        </div>
+        <div className="projectContainer">
+          <div className="project">
+            <img src={pic3} alt="lovely kitchen" />
+            <caption>
+              <h4>Elizabeth Street</h4>
+              <p>Kitchen Remodel</p>
+            </caption>
+          </div>
+        </div>
       </section>
       <section></section>
     </div>
@@ -19,3 +52,25 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+// import React from "react";
+// import "./Portfolio.scss";
+// import PageHeader from "../../components/PageHeader/PageHeader";
+// import Project from "../../components/Project/Project";
+// import { pic3 } from "../../assets/index";
+
+// const Portfolio = () => {
+//   return (
+//     <div className="portfolioContainer">
+//       <PageHeader message="Portfolio" className="portfolio" />
+//       <section className="projectGrid">
+//         {portfolioProjects.map((project) => {
+//           <Project {...project} />;
+//         })}
+//       </section>
+//       <section></section>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
