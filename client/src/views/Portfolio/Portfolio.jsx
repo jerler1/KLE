51e768429021c5f2d3a0debe6cc342b833ed29ed
Bunks <busk_1@yahoo.com>
2021-04-19 20:53:25 -0400

    added dynamic panels for the projects

--- client/src/views/Portfolio/Portfolio.jsx
+++ client/src/views/Portfolio/Portfolio.jsx
@@ -3,50 +3,16 @@ import "./Portfolio.scss";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import Project from "../../components/Project/Project";
 import { portfolioPictures } from "../../assets/images/Portfolio/index";
-import { pic3 } from "../../assets/index";
+
 
 const Portfolio = () => {
-  console.log(portfolioPictures);
   return (
     <div className="portfolioContainer">
       <PageHeader message="Portfolio" className="portfolio" />
       <section className="projectGrid">
-        <div className="projectContainer">
-          <div className="project">
-            <img src={pic3} alt="lovely kitchen" />
-            <caption>
-              <h4>Elizabeth Street</h4>
-              <p>Kitchen Remodel</p>
-            </caption>
-          </div>
-        </div>
-        <div className="projectContainer">
-          <div className="project">
-            <img src={pic3} alt="lovely kitchen" />
-            <caption>
-              <h4>Elizabeth Street</h4>
-              <p>Kitchen Remodel</p>
-            </caption>
-          </div>
-        </div>
-        <div className="projectContainer">
-          <div className="project">
-            <img src={pic3} alt="lovely kitchen" />
-            <caption>
-              <h4>Elizabeth Street</h4>
-              <p>Kitchen Remodel</p>
-            </caption>
-          </div>
-        </div>
-        <div className="projectContainer">
-          <div className="project">
-            <img src={pic3} alt="lovely kitchen" />
-            <caption>
-              <h4>Elizabeth Street</h4>
-              <p>Kitchen Remodel</p>
-            </caption>
-          </div>
-        </div>
+        {portfolioPictures.map((project) => {
+          return <Project {...project} />;
+        })}
       </section>
       <section></section>
     </div>
@@ -54,25 +20,3 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
-
-// import React from "react";
-// import "./Portfolio.scss";
-// import PageHeader from "../../components/PageHeader/PageHeader";
-// import Project from "../../components/Project/Project";
-// import { pic3 } from "../../assets/index";
-
-// const Portfolio = () => {
-//   return (
-//     <div className="portfolioContainer">
-//       <PageHeader message="Portfolio" className="portfolio" />
-//       <section className="projectGrid">
-//         {portfolioProjects.map((project) => {
-//           <Project {...project} />;
-//         })}
-//       </section>
-//       <section></section>
-//     </div>
-//   );
-// };
-
-// export default Portfolio;
