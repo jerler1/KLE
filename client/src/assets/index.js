d958d2e569dd1a679131f07ec2e093439c29a9a4
Bunks <busk_1@yahoo.com>
2021-04-21 00:31:39 -0400

    added imports for images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -14,9 +14,40 @@ export const pic2 = require("./images/Home/pic2-Ellsworth.jpg").default;
 export const pic3 = require("./images/Home/pic1-SanAnselmo.png").default;
 
 // Header images
-export const contactHeader = require("./images/Header/Contact-Whole.png").default;
-export const partnersHeader = require("./images/Header/Partners-Whole.png").default;
-export const portfolioHeader = require("./images/Header/Portfolio-Whole.png").default;
-export const servicesHeader = require("./images/Header/Services-Whole.png").default;
+export const contactHeader = require("./images/Header/Contact-Whole.png")
+  .default;
+export const partnersHeader = require("./images/Header/Partners-Whole.png")
+  .default;
+export const portfolioHeader = require("./images/Header/Portfolio-Whole.png")
+  .default;
+export const servicesHeader = require("./images/Header/Services-Whole.png")
+  .default;
 export const whoHeader = require("./images/Header/WhoWeAre-Whole.png").default;
 export const whyHeader = require("./images/Header/WhyUs-Whole.png").default;
+
+// Services
+export const servicesImages = [
+  {
+    caption: "",
+    image: "",
+    text: "",
+  },
+  {
+    caption: "Lighting Design & Consultation",
+    image: require("./images/Services/01-Consult.png").default,
+    text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
+    We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
+  },
+  {
+    caption: "Lighting Design & Consultation",
+    image: require("./images/Services/01-Consult.png").default,
+    text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
+    We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
+  },
+  {
+    caption: "Lighting Design & Consultation",
+    image: require("./images/Services/01-Consult.png").default,
+    text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
+    We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
+  },
+];
