1cbbbaaa929aef841b0a4d7360b335fea0cf79f7
Bunks <busk_1@yahoo.com>
2021-04-22 00:56:39 -0400

    successfully added image with a linear gradient instead of background image

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./PageHeader.scss";
-// import { contactHeader, partnersHeader, portfolioHeader, servicesHeader, whoHeader, whyHeader } from "../../assets/index";
+import {
+  contactHeader,
+  partnersHeader,
+  portfolioHeader,
+  servicesHeader,
+  whoHeader,
+  whyHeader,
+} from "../../assets/index";
 
 const PageHeader = ({ message, className }) => {
   return (
     <section className={`headerBackgroundImage ${className}`}>
-      {/* <img src={servicesHeader} alt=""/> */}
+      <div className="imageContainer">
+        <img src={servicesHeader} alt="" />
+        <div className="gradientOverlay"></div>
+      </div>
+
       <header className="headerTitle">
         <h1>{message}</h1>
       </header>
