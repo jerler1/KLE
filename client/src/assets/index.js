c9d0e61b57f074e82524d5546a8ec7321dce0d06
Bunks <busk_1@yahoo.com>
2021-04-26 19:43:12 -0400

    added details to the services object

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -36,22 +36,49 @@ export const servicesImages = [
   {
     caption: "Lighting Design & Consultation",
     image: require("./images/Services/01-Consult.png").default,
-    text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
-    We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
-    alt: "",
+    text: `Looking for an expert lighting touch for your new home or remodel? We can work directly with homeowners, architects or general contractors to level up the lighting in your space. 
+    
+    We are adept at CAD and Revit plan revisions and can spec and place all the fixtures, outlets and wiring you’ll need.`,
+    alt: "Electrical Design Papers",
   },
   {
-    caption: "Lighting Design & Consultation",
-    image: require("./images/Services/01-Consult.png").default,
+    caption: "Complete Design Build Electrical",
+    image: require("./images/Services/02-CompleteDesignBuild.png").default,
     text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
+
     We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
-    alt: "",
+    alt: "Well Lighted Building",
   },
   {
-    caption: "Lighting Design & Consultation",
-    image: require("./images/Services/01-Consult.png").default,
-    text: `Building your dream home? Lighting design is essential for making your new house feel like a home. Whatever your vision, we can work with you to make it a reality. 
-    We provide expertise on current fixture trends, as well as size, placement, smart home additions, outlet positioning, and everything else to complete a lighting design system in your new home. Then our team of certified electricians put our plans into place.`,
-    alt: "",
+    caption: "Smart Home Installs",
+    image: require("./images/Services/03-SmartHome.png").default,
+    text: `There are great benefits to integrating technology into your home. Whether it’s solar paneling, car chargers, auto-initiating generators, or remote thermostats and light switches, we’ve got you covered. 
+    
+    Certified Lutron Pro Installer.`,
+    alt: "Solar Panels",
+  },
+  {
+    caption: "Underground Service Conversions",
+    image: require("./images/Services/04-Underground.png").default,
+    text: `Is your view blocked by unsightly electrical wires? Let’s get those wires out of the sky and into the ground so you can enjoy your home regardless of which direction you look.`,
+    alt: "Power Lines",
+  },
+  {
+    caption: "Safety Inspections",
+    image: require("./images/Services/05-Safety.png").default,
+    text: `There are many reasons you might need a safety inspection before beginning work. Buying a new house and you want to make sure everything’s safe? Condo conversion? They require a private inspection. Working on an older home and not sure what you need or whether it’s safe?`,
+    alt: "Line Man Inspecting",
+  },
+  {
+    caption: "Service Upgrades",
+    image: require("./images/Services/06-ServiceUpgrade.png").default,
+    text: `Do you need more power for newer appliances? What about electric vehicles? We can get your home ready to accommodate whatever you throw at.`,
+    alt: "Poorly Managed Wires Inside A Wall",
+  },
+  {
+    caption: "Fixture Installations",
+    image: require("./images/Services/07-fixtureinstall.png").default,
+    text: `Looking for the perfect fixture to brighten up a room? We work directly with homeowners, or with interior designers to source the perfect fixtures and install them as well.`,
+    alt: "Lighting Fixture",
   },
 ];
