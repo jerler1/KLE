390d66831a5d89eec43ee264c57653512b449c1a
Bunks <busk_1@yahoo.com>
2021-04-27 00:00:29 -0400

    added ternary operators to the mouse enter and exit for mobile - so they are ignored

--- client/src/components/ServiceCard/ServiceCard.jsx
+++ client/src/components/ServiceCard/ServiceCard.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import "./ServiceCard.scss";
 
 const ServiceCard = ({ caption, shortDescription, image, text, alt }) => {
   const [isActive, setIsActive] = useState(false);
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 950px)" });
 
   const onClick = () => {
     setIsActive(!isActive);
@@ -13,12 +15,21 @@ const ServiceCard = ({ caption, shortDescription, image, text, alt }) => {
       <div
         className="service"
         onClick={onClick}
-        onMouseEnter={() => {
-          setIsActive(false);
-        }}
-        onMouseLeave={() => {
-          setIsActive(false);
-        }}
+        onMouseEnter={
+          isTabletOrMobile
+            ? () => false
+            : () => {
+                setIsActive(true);
+              }
+        }
+        onMouseLeave={
+          isTabletOrMobile
+            ? () => false
+            : () => {
+                console.log("click");
+                setIsActive(false);
+              }
+        }
       >
         <img src={image} alt={alt} />
         <div className={isActive ? "fullScreenCaption" : "miniServiceCaption"}>
