96558c5a8a014074829bebcf889d441f86be2181
Bunks <busk_1@yahoo.com>
2021-05-01 20:07:36 -0400

    added react image gallery

--- client/src/views/Portfolio/Portfolio.jsx
+++ client/src/views/Portfolio/Portfolio.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Portfolio.scss";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import Project from "../../components/Project/Project";
 import gloomyPicture from "../../assets/images/CFA/PortfolioCTA.png";
 import { portfolioPictures } from "../../assets/images/Portfolio/index";
 import Button from "../../components/Button/Button";
-
+import ImageGallery from "react-image-gallery";
 
 const Portfolio = () => {
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    console.log("hi hi");
+  }, [images]);
+
+  const settingImages = (e) => {
+    setImages(portfolioPictures[0].pictures);
+    
+    console.log("Setting Images");
+    console.log(e.currentTarget);
+  }
+
+  // boolean for if gallery is being shown
+  // -- if false set images to null
+  // -- if null display the portfolio cards
   return (
     <div className="portfolioContainer">
-      <PageHeader message="Portfolio" className="portfolio" absoluteURL="Portfolio-Whole"/>
-      <section className="projectGrid">
-        {portfolioPictures.map((project) => {
-          return <Project {...project} />;
+      <PageHeader
+        message="Portfolio"
+        className="portfolio"
+        absoluteURL="Portfolio-Whole"
+      />
+      <section className={images ? "imageGallery" :"projectGrid"}>
+        {images ? <ImageGallery images={images}/> : portfolioPictures.map((project, index) => {
+          return <Project key={index} {...project} settingImages={settingImages} />;
         })}
       </section>
       <section className="contactCFAContainer">
         <div className="gloomyPicture">
-          <img src={gloomyPicture} alt="gloomy kitchen"/>
+          <img src={gloomyPicture} alt="gloomy kitchen" />
         </div>
         <div className="cfaTextWrapper">
           <div className="cfaText">
             <p>Ready to make your space beautiful?</p>
             {/* todo: link button */}
-            <Button className="button primaryButton">
-              Get in Touch
-            </Button>
+            <Button className="button primaryButton">Get in Touch</Button>
           </div>
         </div>
       </section>
