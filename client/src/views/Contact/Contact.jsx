edf08be1cd8b3a13924643ac21c3a89c35627d1d
Bunks <busk_1@yahoo.com>
2021-05-03 00:32:58 -0400

    finished the html and css of desktop version

--- client/src/views/Contact/Contact.jsx
+++ client/src/views/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
-import { email, phone } from "../../assets/index";
+// import { email, phone } from "../../assets/index";
 import Icon from "../../components/Icon/Icon";
 
 import "./Contact.scss";
@@ -29,6 +29,27 @@ const Contact = () => {
         <div className="contactFormWrapper">
           <div className="contactFormContent">
             <h3>Send us a message</h3>
+            <form className="contactForm">
+              <div className="emailInputWrapper">
+                <label className="label">Email</label>
+                <input className="input" type="text" name="email" />
+              </div>
+              <div className="phoneInputWrapper">
+                <label className="label">Phone</label>
+                <input className="input" type="text" name="phone" />
+              </div>
+              <div className="timeInputWrapper">
+                <label className="label">Best time to contact you</label>
+                <input className="input" type="text" name="time" />
+              </div>
+              <div className="messageInputWrapper">
+                <label className="label">Your Message</label>
+                <textarea className="input" type="text" name="message" />
+              </div>
+              <div className="buttonWrapper">
+                <button className="button primaryButton">Send</button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
