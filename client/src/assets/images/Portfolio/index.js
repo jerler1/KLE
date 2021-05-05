d90baa221baaf0c62d5ad48331a9218bbfc98f75
Bunks <busk_1@yahoo.com>
2021-05-04 21:03:03 -0400

    major refactor of importall and removed the array in pictures

--- client/src/assets/images/Portfolio/index.js
+++ client/src/assets/images/Portfolio/index.js
@@ -1,13 +1,6 @@
 function importAll(r) {
   return r.keys().map(r);
 }
-// function importAll(r) {
-//   let images = {};
-//   r.keys().map((item, index) => {
-//     return images[item.replace("./", "")] = r(item);
-//   });
-//   return images;
-// }
 
 export const portfolioPictures = [
   {
@@ -24,37 +17,35 @@ export const portfolioPictures = [
     name: "506th Street",
     shortDescription: "Short",
     mainPicture: require("./576th-Street/main.jpg").default,
-    pictures: [
-      importAll(require.context("./576th-Street", false, /\.(png|jpe?g|svg)$/)),
-    ],
+    pictures: importAll(
+      require.context("./576th-Street", false, /\.(png|jpe?g|svg)$/)
+    ),
   },
   {
     key: "Ellsworth",
     name: "Ellsworth Street",
     shortDescription: "Short",
     mainPicture: require("./Ellsworth-Street/EllsworthDusksetX.jpg").default,
-    pictures: [
-      importAll(
-        require.context("./Ellsworth-Street", false, /\.(png|jpe?g|svg)$/)
-      ),
-    ],
+    pictures: importAll(
+      require.context("./Ellsworth-Street", false, /\.(png|jpe?g|svg)$/)
+    ),
   },
   {
     key: "Anselmo",
     name: "San Anselmo",
     shortDescription: "Short",
     mainPicture: require("./San-Anselmo/Raymond_Kitch.png").default,
-    pictures: [
-      importAll(require.context("./San-Anselmo", false, /\.(png|jpe?g|svg)$/)),
-    ],
+    pictures: importAll(
+      require.context("./San-Anselmo", false, /\.(png|jpe?g|svg)$/)
+    ),
   },
   {
     key: "Shasta",
     name: "Shasta",
     shortDescription: "Short",
     mainPicture: require("./Shasta/Bocci_HexTile.jpeg").default,
-    pictures: [
-      importAll(require.context("./Shasta", false, /\.(png|jpe?g|svg)$/)),
-    ],
+    pictures: importAll(
+      require.context("./Shasta", false, /\.(png|jpe?g|svg)$/)
+    ),
   },
 ];
