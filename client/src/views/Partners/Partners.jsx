a2585b4d589e5ada691e7b6f33932c8d83529397
Bunks <busk_1@yahoo.com>
2021-05-12 15:15:58 -0400

    added headers for the partner sections

--- client/src/views/Partners/Partners.jsx
+++ client/src/views/Partners/Partners.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 import "./Partners.scss";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import CallForAction from "../../components/CallForAction/CallForAction";
 
 const Partners = () => {
-    return (
-        <div>
-            
+  let history = useHistory();
+
+  const onClick = () => {
+    history.push("/contact-us");
+  };
+
+  return (
+    <div className="partnersContainer">
+      <PageHeader
+        message="Our Partners"
+        absoluteURL="Partners-Whole"
+        alignment="topAligned"
+        gradientAlignment="gradientOverlayTopLeft"
+      />
+      <section className="whoWeWorkWithWrapper">
+        <div className="whoWeWorkWithHeader">
+          <h4>We work with the best</h4>
         </div>
-    );
+        <div className="whoWeWorkWithGrid"></div>
+      </section>
+      <section className="bikeFriendsWrapper">
+        <div className="bikeFriendsHeader">Bike Friends</div>
+      </section>
+      <CallForAction
+        alt="Toasting of drinks."
+        message="Looking to partner with us?"
+        buttonMessage="Get in Touch"
+        pictureSource="partners"
+        onClick={onClick}
+      />
+    </div>
+  );
 };
 
 export default Partners;
